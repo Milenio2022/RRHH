0467fd39746b77a6f9a1e5cf2ad465a3b6266330
sebasT69 <sebasfer2504@outlook.com>
2023-09-22 22:51:17 -0500

    Dashboard empleado

---
+++ dashboard.php
@@ -0,0 +1,68 @@
+<nav class="nav">
+        <ul class="list">
+            <li class="list__item">
+                <div class="list__button">
+                    <img src="assets/dashboard.svg" class="list__img">
+                    <a href="#" class="nav__link">Inicio</a>
+                </div>
+            </li>
+
+            <li class="list__item list__item--click">
+                <div class="list__button list__button--click">
+                    <img src="assets/doc.svg" class="list__img">
+                    <a href="#" class="nav__link">Empleado</a>
+                    <img src="assets/arrow.svg" class="list__arrow">
+                </div>
+
+                <ul class="list__show">
+                    <li class="list__inside">
+                        <a href="#" class="nav__link nav__link--inside">Agregar Empleado</a>
+                    </li>
+
+                    <li class="list__inside">
+                        <a href="#" class="nav__link nav__link--inside">Ver Empleado</a>
+                    </li>
+                </ul>
+
+            </li>
+
+
+            <li class="list__item">
+                <div class="list__button">
+                    <img src="assets/stadistic.svg" class="list__img">
+                    <a href="#" class="nav__link">Servicio</a>
+                </div>
+            </li>
+
+            <li class="list__item list__item--click">
+                <div class="list__button list__button--click">
+                    <img src="assets/notific.svg" class="list__img">
+                    <a href="#" class="nav__link">Permiso</a>
+                    <img src="assets/arrow.svg" class="list__arrow">
+                </div>
+
+                <ul class="list__show">
+                    <li class="list__inside">
+                        <a href="#" class="nav__link nav__link--inside">Estoy dentro</a>
+                    </li>
+
+                    <li class="list__inside">
+                        <a href="#" class="nav__link nav__link--inside">Estoy dentro</a>
+                    </li>
+
+                    <li class="list__inside">
+                        <a href="#" class="nav__link nav__link--inside">Estoy dentro</a>
+                    </li>
+                </ul>
+
+            </li>
+
+
+            <li class="list__item">
+                <div class="list__button">
+                    <img src="assets/mensaje.svg" class="list__img">
+                    <a href="#" class="nav__link">Configuración</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
