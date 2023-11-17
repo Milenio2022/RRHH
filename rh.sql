3e208faeae4398d808195b6c1643c56f4a81f78e
sebasT69 <sebasfer2504@outlook.com>
2023-11-16 20:12:08 -0500

    BD mejorada

---
+++ rh.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-11-2023 a las 02:09:57
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `beneficios`
+--
+
+CREATE TABLE `beneficios` (
+  `id` int(11) NOT NULL,
+  `tipo_beneficio` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cargo`
+--
+
+CREATE TABLE `cargo` (
+  `id` int(11) NOT NULL,
+  `nombre_cargo` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `id` int(11) NOT NULL,
+  `nombre_departamento` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `fecha_nacimiento` datetime DEFAULT NULL,
+  `genero` varchar(10) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL,
+  `fecha_contratacion` datetime DEFAULT NULL,
+  `salario` float DEFAULT NULL,
+  `cargo` varchar(255) DEFAULT NULL,
+  `departamento` varchar(255) DEFAULT NULL,
+  `id_supervisor` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  `id_cargo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado_beneficio`
+--
+
+CREATE TABLE `empleado_beneficio` (
+  `id_empleado` int(11) NOT NULL,
+  `id_beneficio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_laboral`
+--
+
+CREATE TABLE `historial_laboral` (
+  `id` int(11) NOT NULL,
+  `fecha_inicio` datetime DEFAULT NULL,
+  `fecha_finalizacion` datetime DEFAULT NULL,
+  `id_empleado` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `beneficios`
+--
+ALTER TABLE `beneficios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_supervisor` (`id_supervisor`),
+  ADD KEY `id_departamento` (`id_departamento`),
+  ADD KEY `id_cargo` (`id_cargo`);
+
+--
+-- Indices de la tabla `empleado_beneficio`
+--
+ALTER TABLE `empleado_beneficio`
+  ADD PRIMARY KEY (`id_empleado`,`id_beneficio`),
+  ADD KEY `id_beneficio` (`id_beneficio`);
+
+--
+-- Indices de la tabla `historial_laboral`
+--
+ALTER TABLE `historial_laboral`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_supervisor`) REFERENCES `supervisor` (`id`),
+  ADD CONSTRAINT `empleado_ibfk_2` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`),
+  ADD CONSTRAINT `empleado_ibfk_3` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id`);
+
+--
+-- Filtros para la tabla `empleado_beneficio`
+--
+ALTER TABLE `empleado_beneficio`
+  ADD CONSTRAINT `empleado_beneficio_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`),
+  ADD CONSTRAINT `empleado_beneficio_ibfk_2` FOREIGN KEY (`id_beneficio`) REFERENCES `beneficios` (`id`);
+
+--
+-- Filtros para la tabla `historial_laboral`
+--
+ALTER TABLE `historial_laboral`
+  ADD CONSTRAINT `historial_laboral_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
