43f6af21a65bebf81d8a331be263f3f53a55662a
YUMIUKI <144287008+YUMIUKI@users.noreply.github.com>
2023-11-19 22:29:24 -0500

    Update permiso.php

--- usuario/permiso.php
+++ usuario/permiso.php
@@ -4,21 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Mi Menú</title>
 </head>
 
 <body>
-    <div class="menu">
-        <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Perfil</a></li>
-            <li><a href="#">Permiso</a></li>
-            <li><a href="aplicarpermiso.php">Aplicar</a></li>
-            <li><a href="#">Aplicaciones</a></li>
-            <li><a href="#">contraseña</a></li>
-        </ul>
-    </div>
+    <?php 
+        include '../includes/head.php'
+    ?>
     <!-- Resto de tu contenido -->
     <h1>SOLICITUDES DE PERMISO</h1>
 
