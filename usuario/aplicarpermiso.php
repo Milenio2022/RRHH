b831e976b1a9beec2ecc24b16854221cc075cc28
YUMIUKI <144287008+YUMIUKI@users.noreply.github.com>
2023-11-22 12:20:28 -0500

    Update aplicarpermiso.php

--- usuario/aplicarpermiso.php
+++ usuario/aplicarpermiso.php
@@ -1,21 +1,39 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/styles.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <title>Mi Menú</title>
+    <link rel="stylesheet" href="../css/stilos.css">
+    <title>Solicitud de Permiso</title>
 </head>
 
 <body>
-    <?php 
-        include '../includes/head.php'
-    ?>
-    <!-- Resto de tu contenido -->
-    <h1>APLICACION DE PERMISO</h1>
+    <?php include '../includes/head.php' ?>
+    
+    <div class="contenido">
+        <h1>Solicitud de Permiso</h1>
 
+        <form method="post" action="procesar_permiso.php">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" required>
+
+            <label for="motivo">Motivo:</label>
+            <textarea name="motivo" rows="4" required></textarea>
+
+            <label for="fecha_inicio">Fecha de Inicio:</label>
+            <input type="date" name="fecha_inicio" required>
+
+            <label for="fecha_fin">Fecha de Fin:</label>
+            <input type="date" name="fecha_fin" required>
+
+            <!-- Puedes tener un campo oculto para el estado si ya tiene un valor predeterminado -->
+            <input type="hidden" name="estado" value="Pendiente">
+
+            <input type="submit" value="Enviar Solicitud">
+        </form>
+    </div>
 
 </body>
 
